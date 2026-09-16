b50e3da893c6f1401519c0c2f30aea4043d78a7a
Jevv <itsmejevjev@gmail.com>
2026-09-16 16:39:55 +0800

    coment

--- src/App.jsx
+++ src/App.jsx
@@ -19,13 +19,13 @@ function App() {
           <div className="hero-image">
             <img src="C:\Users\User\Downloads\˖⁺‧₊˚♡˚₊‧⁺˖.jfif" alt="Hero Image" />
           </div>
-          
+
           <div className="cta-row">
             <button className="btn btn-primary btn-lg">Get started</button>
             <button className="btn btn-outline-light btn-lg">View work</button>
           </div>
 
-
+{/* gsggsgs */}
         </section>
 
         <div className="row g-4 mt-1">
